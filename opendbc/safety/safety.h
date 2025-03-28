b40f3fa2b38167d80b86c266a89835ee65a1e198
mx <599590980@qq.com>
2025-03-29 00:06:08 +0800

    feat(car): Sync byd brand from yysnet

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -21,6 +21,7 @@
 #include "opendbc/safety/modes/nissan.h"
 #include "opendbc/safety/modes/volkswagen_mqb.h"
 #include "opendbc/safety/modes/volkswagen_pq.h"
+#include "opendbc/safety/modes/byd.h"
 #include "opendbc/safety/modes/elm327.h"
 #include "opendbc/safety/modes/body.h"
 
@@ -419,6 +420,7 @@ int set_safety_hooks(uint16_t mode, uint16_t param) {
     {SAFETY_BODY, &body_hooks},
     {SAFETY_FORD, &ford_hooks},
     {SAFETY_RIVIAN, &rivian_hooks},
+    {SAFETY_BYD, &byd_hooks},
 #ifdef CANFD
     {SAFETY_HYUNDAI_CANFD, &hyundai_canfd_hooks},
 #endif
